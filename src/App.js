49362a087554f975b4b8a1ae18f20a4a84618052
manafg <manafhgh22@gmaail.com>
2018-10-14 01:25:19 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login'
+import {BrowserRouter , Route, Switch } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Currencies from './Components/Currencies/Currencies';
+import Error from './Components/Error/Error'
+
 class App extends Component {
   style = {
     marginTop: '20'
   }
   render() {
     return (
-      <div className="App">
-        <Login />
-      </div>
+      <BrowserRouter>
+        <Switch>
+            <Route path='/' component= {Login} exact/>
+            <Route path='/Home' component= { Home } exact/>
+            <Route path='/Currencies' component = { Currencies }/>
+            <Route component={Error} />
+        </Switch>
+       </BrowserRouter>
     );
   }
 }
